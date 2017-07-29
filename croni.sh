03fc1c6442019052411e9e038e32833648003ec2
André Boddenberg <andre@boddenberg.it>
2017-07-29 13:17:57 +0200

    Introduce upgrade, move code around, still not tested

--- croni.sh
+++ croni.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
 function log() {
-	echo "[$(date)] $1" >> "$base/../logs/croni.log"
+	echo "[$(date)] $1" >> "$base/croni_logs/croni.log"
 }
 
-function deploy_job() {
-	croni="$(cat "$base/$1/$2" | grep "croni\=" | cut -d "\"" -f2)"
-	if [ "$croni" = "" ]; then
-		log "[ERROR] No croni variable declared in $base/$2/$3"
-		echo "# $croni $submodule_base/croni.sh run $1 $2" >> $new_crontab
-	else
-		echo "$croni $submodule_base/croni.sh run $1 $2" >> $new_crontab
+function init() {
+
+	mkdir -p $base/croni_logs/
+
+	if [ ! -f "$HOME/.croni" ]; then
+		sudo cp "/var/spool/cron/crontabs/$USER" "$HOME/crontab.bak"
+		sudo chown $USER:$USER "$HOME/crontab.bak"
+
+		touch "$HOME/.croni"
+		chmod o-r "$HOME/.croni"
+		sudo rm "/var/spool/cron/crontabs/$USER"
+		sudo ln -s "$HOME/.croni" "/var/spool/cron/crontabs/$USER"
 	fi
 
-	job_logs="$base/../logs/$1/$2"
-	job_logs="${job_logs//.sh/}"
-	mkdir -p "$job_logs"
+	if [ ! -f "croni.html" ]; then
+		ln -s croni/webroot/index.html croni.html
+	fi
+
+	if [ ! -f "croni.html" ]; then
+		ln -s croni/croni.sh croni.sh
+	fi
+
+	deploy
 }
 
 function deploy() {
+
+	mkdir -p $base/croni_logs/
 
 	old_crontab="$HOME/.croni"
 	new_crontab="$HOME/.croni_new"
 
 	echo "" > $new_crontab
 	echo "# croni gererated crontab (https://git.boddenberg.it/croni)" >> $new_crontab
-	#TODO:
+	# TODO: load cron expression from croni.cfg
 	echo "0 5,17 * * * $submodule_base/croni.sh update" >> $new_crontab
 	echo "" >> $new_crontab
 
@@ -48,15 +61,23 @@ function deploy() {
 	cp "$new_crontab" "$old_crontab"
 }
 
-function initialise() {
-	# TODO: check whether .croni exists
-	sudo cp "/var/spool/cron/crontabs/$USER" "$HOME/crontab.bak"
-	chown $USER:$USER "$HOME/crontab.bak"
+function deploy_job() {
 
-	touch "$HOME/.croni"
-	sudo ln -s "$HOME/.croni" "/var/spool/cron/crontabs/$USER"
+	croni="$(cat "$base/$1/$2" | grep "croni\=" | cut -d "\"" -f2)"
+
+	if [ "$croni" = "" ]; then
+		log "[ERROR] No croni variable declared in $base/$2/$3"
+		echo "# $croni $submodule_base/croni.sh run $1 $2" >> $new_crontab
+	else
+		echo "$croni $submodule_base/croni.sh run $1 $2" >> $new_crontab
+	fi
+
+	job_logs="$base/cron_logs/$1/$2"
+	job_logs="${job_logs//.sh/}"
+	mkdir -p "$job_logs"
 }
 
+### ###
 function run() {
 	project="$1"
 	job="$2"
@@ -74,10 +95,10 @@ function run() {
 
 	date="$(date +%y-%m-%w_%H:%m:%S)"
 	log "Starting build: $project/$job number: $next_bn"
-	job_log="$job_dir/${job}_${next_bn}_${date}"
+	job_log="$job_dir/${job}_${date}_${next_bn}.log"
 
 	start=$(date +%s)
-	/bin/bash -ex "$base/$project/$job" 2&>1 > "$job_log"
+	/bin/sh -ex "$base/$project/$job" 2&>1 > "$job_log"
 	exit_code=$?
 	stop=$(date +%s)
 	duration=$((stop-start))
@@ -97,21 +118,45 @@ function run() {
 	fi
 }
 
-function update() {
-	exits=0
-	cd $base || exits=$((exits+$?))
-	git fetch origin; exits=$((exits+$?))
-	git rebase origin/master; exits=$((exits+$?))
-	deploy; exits=$((exits+$?))
+# UPDATE & UPGRADE
+function revision() {
+	cd "$base/$1" || exit
+	echo "$(git rev-parse HEAD)"
+}
 
-	if [ "$exits" -gt 0 ]; then
-		log "Updating jobs from git repository failed: $exits"
+function upgrade() {
+	old_head="$(revision croni)"
+	cd $base || exit
+	git submodule update --remote
+	new_head="$(revision croni)"
+
+	if [ "$old_head" != "$new_head" ]; then
+		log "Upgrade: croni has been updated old: $old_head new: $new_head"
 	else
-		log "Updating jobs from git repository succeeded"
+		log "Upgrade: nothing changed. currrent HEAD: $new_head"
 	fi
 }
 
+function update() {
+	cd $base || exit
+
+	old_head="$(revision)"
+
+	git fetch origin
+	git rebase origin/master
+	if [ $? -gt 0 ]; then
+		log "update call: rebase failed, stashing possible local changes before retrying"
+		git stash
+		git rebase origin/master
+	fi
+
+	new_head="$(revision)"
+
+	if []
+	deploy
+}
+
 submodule_base="$(dirname "$(readlink -f $0)")"
-base="${submodule_base//.croni/croni}"
+base="${submodule_base}/../"
 export base submodule_base
 $@
